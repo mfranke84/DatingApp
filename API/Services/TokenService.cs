d12c623f1b6ec1231cc8232cd6d901c3375aa507
Markus Franke <markus.dirk.franke@volkswagen-infotainment.com>
2026-02-17 10:19:47 +0100

    Implementing the token logic

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,5 +1,10 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
+using System.Text;
+using API.Entities;
 using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services;
 
@@ -10,6 +15,26 @@ public class TokenService(IConfiguration config) : ITokenService
         var tokenKey = config["TokenKey"] ?? throw new Exception ("Can not get key.");
         if (tokenKey.Length < 64) 
             throw new Exception("Your token key needs to be >= 64 characters.");
-        var key = new SymmetricSecurityKey()
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Email, user.Email),
+            new(ClaimTypes.NameIdentifier, user.Id.ToString())
+        };
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
     }
 }
