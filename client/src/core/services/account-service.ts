ba4f08ca4df8cc62c69ecc99d525db6d7bbb66c2
Markus Franke <markus.dirk.franke@volkswagen-infotainment.com>
2026-07-09 18:14:56 +0200

    Completed registration

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { User } from '../../types/user';
+import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs';
 
 @Injectable({
@@ -13,15 +13,31 @@ export class AccountService {
 
   baseUrl = "https://localhost:5001/api/"
 
-  login(creds: any){
+  register(creds: RegisterCreds){
+    return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
+      tap( user => {
+        if(user){
+          this.setCurrentUser(user);
+        }
+      })
+    )
+  }
+
+  login(creds: LoginCreds){
     return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
       tap( user => {
         if(user){
-          this.currentUser.set(user);
-          localStorage.setItem('user', JSON.stringify(user));
-      }
+          this.setCurrentUser(user);
+        }
       })
     )
+  }
+  
+
+  setCurrentUser(user: User){
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUser.set(user);
+    
   }
   
   logout(){
